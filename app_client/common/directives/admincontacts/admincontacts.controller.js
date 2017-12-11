95e88836fabb06b3a4b0d52413940bec21a0e1a8
jahid haque <haque.jahid@gmail.com>
2017-12-11 14:07:58 +0000

    updates

--- app_client/common/directives/admincontacts/admincontacts.controller.js
+++ app_client/common/directives/admincontacts/admincontacts.controller.js
@@ -11,23 +11,38 @@
 		.module('etaxi')
 		.controller('adminContactsCtrl', adminContactsCtrl);
 
-	adminContactsCtrl.$inject = ['userservice'];
+	adminContactsCtrl.$inject = ['userservice', 'authentication'];
 
-	function adminContactsCtrl(userservice) {
+	function adminContactsCtrl(userservice, authentication) {
 		const adcon = this;
+
+		adcon.ContactType = authentication.currentUser().account_type;
+
+		if (adcon.ContactType === 'admin') {
+			adcon.loadAllContact = true;
+			adcon.loadAccountantContact = true;
+			adcon.loadCustomerContact = false;
+		}
+		else if (adcon.ContactType === 'accountant') {
+			adcon.loadAllContact = false;
+			adcon.loadCustomerContact = true;
+			adcon.loadAccountantContact = false;
+		}
 
 		// following function will get all user email address
 		adcon.loadAdminContacts = () => {
 			userservice
 				.adminContact()
-				.then((response) => {
-					console.log('response', response);
+				.then((response) => {							
+					adcon.loadAccountantContact = true;
 					adcon.customers = response.data.contacts.filter((customer) => {
 						return customer.account_type === 'customer';
 					});
+					
 					adcon.accountants = response.data.contacts.filter((accountant) => {
 						return accountant.account_type === "accountant";
 					})
+					
 				})
 				.catch((err) => {
 					alert(err);
